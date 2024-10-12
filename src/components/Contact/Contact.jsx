8d6cc46be5ff4075b31929561ae3360ef755ac58
anaskhan3458027 <anaskhan3458027@gmail.com>
2024-10-13 00:28:01 +0530

    1.4

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="anaskhan3458027@gmail.com">anaskhan3458027@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/mohd-anas-khan-7179a9240">https://www.linkedin.com/in/mohd-anas-khan-7179a9240</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://www.github.com/anaskhan3458027">github.com/anaskhan3458027</a>
         </li>
       </ul>
     </footer>
